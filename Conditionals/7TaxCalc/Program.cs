ec88797dfa706e328280b8553d4b81d8213858de
Sam Cooper <sam_cooper101@proton.me>
2023-10-13 14:14:35 +0100

    even more tasks

---
+++ Conditionals/7TaxCalc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _7TaxCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your yearly income");
+            int income = int.Parse(Console.ReadLine());
+            if (income > 150000)
+            {
+                Console.WriteLine("you will pay £" + (income * 0.45)+" in tax");
+            }else if (income > 50270)
+            {
+                Console.WriteLine("you will pay £" + (income * 0.4) + " in tax");
+            }else if(income>12570)
+            {
+                Console.WriteLine("you will pay £" + (income * 0.2) + " in tax");
+            }
+            else
+            {
+                Console.WriteLine("You will pay no tax");
+            }
+        }
+    }
+}
