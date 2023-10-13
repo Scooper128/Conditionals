378b987fd04071867ca130004cf3dc5c11a529c9
Sam Cooper <sam_cooper101@proton.me>
2023-10-13 12:09:48 +0100

    Pizza Shop

---
+++ Conditionals/2PizzaShop/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2PizzaShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            This pizza shop application asks the user for the value of their food in pounds.
+ 
+            If the value of the food is at least £10 then they can get free delivery - otherwise you need to charge an extra £2 for delivery.
+ 
+            If the value of the food is more than £14 then they can get free delivery and a free garlic bread.
+            */
+
+            Console.WriteLine("Welcome to the Pizza Shop Application!");
+
+            Console.WriteLine("What was the value of your order?");
+            float val = float.Parse(Console.ReadLine());
+            if (val >= 14)
+            {
+                Console.WriteLine("Your cost will be " + val + " and you get free delivery and a free garlic bread");
+            }else if (val >= 10)
+            {
+                Console.WriteLine("your cost will be " + val + " and you get free delivery");
+            }
+            else
+            {
+                Console.WriteLine("your cost is "+(val+2));
+            }
+
+        }
+    }
+}
---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace OldestSibiling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 Oldest Siblings");
+
+            Console.WriteLine("Welcome to the Oldest Sibling Application.");
+
+            Console.WriteLine("This application will tell us who the oldest of a pair of siblings is.");
+
+            Console.WriteLine("First sibling, what is your name?");
+
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name1 + ", How old are you?");
+
+            int age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second sibling, what is your name?");
+
+            string name2 = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name2 + ", How old are you?");
+
+            int age2 = int.Parse(Console.ReadLine());
+
+            if (age1 > age2)
+            {
+                Console.WriteLine(name1 + " is older by " + (age1 - age2) + " years");
+            }
+            else if (age2 > age1)
+            {
+                Console.WriteLine(name2 + " is older by " + (age2 - age1) + " years");
+            }
+            else
+            {
+                Console.WriteLine("Neither are older");
+            }
+        }
+    }
+}
